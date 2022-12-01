28916cce2dbebb5579f88f100d684e0f52064120
Kirill_arsentev <arsen@DESKTOP-4AJNJ1I>
2022-12-01 17:01:45 +0300

    Добавьте файлы проекта.

---
+++ 3.12.22/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._12._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = "note1.txt";
+            //using (StreamWriter writer = new StreamWriter(path, true))
+            //{
+            //    writer.WriteLineAsync("Addition");
+            //    writer.WriteAsync("4,5");
+            //}
+
+            string path1 = "note1.txt";
+            // асинхронное чтение
+            using (StreamReader reader = new StreamReader(path1))
+            {
+                string text = reader.ReadToEnd();
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
